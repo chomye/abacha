a5ca81653fd0c6ac9c217705e06e22e53ce1e27d
chomye <viviancezike@gmail.com>
2020-02-13 12:20:59 +0100

    created an online database

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -9,7 +9,7 @@
 
 
 //for online hosting
-define("DB_SERVER","localhost");
+define("DB_SERVER","db4free.net:3306");
 define("DB_USER","chiomavvian20");
 define("DB_PASSWORD","izuka@43");
 define("DB_DATABASE","chioma20");
