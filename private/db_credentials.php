9c2df05cf67e4d8f6a6b3c5d4d89e4ad1fc56e8e
chomye <viviancezike@gmail.com>
2020-02-13 12:09:59 +0100

    created an online database

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,15 +1,18 @@
 <?php
 //defining my db credentials
 
+// define("DB_SERVER","localhost");
+// define("DB_USER","root");
+// define("DB_PASSWORD","");
+// define("DB_DATABASE","abachanation");
+
+
+
+//for online hosting
 define("DB_SERVER","localhost");
-define("DB_USER","root");
-define("DB_PASSWORD","");
-define("DB_DATABASE","abachanation");
-
-
-
-
-
+define("DB_USER","chiomavvian20");
+define("DB_PASSWORD","izuka@43");
+define("DB_DATABASE","chioma20");
 
 
 
